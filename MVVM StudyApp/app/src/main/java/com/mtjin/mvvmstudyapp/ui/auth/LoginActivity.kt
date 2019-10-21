9b8ef3de2b72db0504c4a682e0616c37d5458ba8
seungeon <tmddjs210@naver.com>
2019-10-21 22:50:41 +0900

    MVVM 코틀린 공부시작 및 프로젝트 세팅 login, signup 액티비티 생성

---
+++ MVVM StudyApp/app/src/main/java/com/mtjin/mvvmstudyapp/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.mtjin.mvvmstudyapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mtjin.mvvmstudyapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
